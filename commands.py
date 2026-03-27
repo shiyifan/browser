a2514fd752e9445bfb498ed4c0eb5951b32acb0f
Ivan Shi <shiyifan@live.cn>
2026-03-27 20:24:24 +0800

    update

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@
 
 
 class DrawText:
+    # (x1, y1)为相对于canvas的坐标
     def __init__(self, x1, y1, text, font):
         self.left = x1
         self.top = y1
@@ -12,6 +13,7 @@ class DrawText:
         # 表示当前行的底部纵坐标，用于判断绘制位置是否位于canvas的可见区域外
         self.bottom = y1 + font.metrics("linespace")
 
+    # scroll: 已向上滚动的距离
     def execute(self, scroll, canvas):
         canvas.create_text(
             self.left, self.top - scroll, text=self.text, font=self.font, anchor="nw"
@@ -19,6 +21,7 @@ class DrawText:
 
 
 class DrawRect:
+    # (x1, y1), (x2, y2)为相对于canvas的坐标
     def __init__(self, x1, y1, x2, y2, color):
         self.top = y1
         self.left = x1
@@ -26,6 +29,7 @@ class DrawRect:
         self.right = x2
         self.color = color
 
+    # scroll: 已向上滚动的距离
     def execute(self, scroll, canvas):
         canvas.create_rectangle(
             self.left,
